06d14fffc1eddfadbbfaaa0b8c921f0bbe01a04c
Alex Dmitrevskiy <a.dmitrevskiy@gmail.com>
2017-10-27 19:22:42 +0300

    Async load for currencies

---
+++ app/src/main/java/com/example/tfs_exchange/AsyncDBLoader.java
@@ -0,0 +1,107 @@
+package com.example.tfs_exchange;
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.AsyncTaskLoader;
+import android.util.Log;
+
+import com.example.tfs_exchange.Currency;
+import com.example.tfs_exchange.DBHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pusya on 23.10.17.
+ */
+
+public class AsyncDBLoader extends AsyncTaskLoader<List<Currency>> {
+
+
+    private DBHelper dbHelper;
+
+    public static final String TAG = "AsyncLoader";
+
+    public AsyncDBLoader(Context context) {
+        super(context);
+        Log.d(TAG, hashCode() + " create AsyncLoader");
+    }
+
+    @Override
+    public List<Currency> loadInBackground() {
+         List<Currency> currencies = new ArrayList<Currency>();
+
+        //подключаем Data Base Helper, получаем из него БД для чтения
+        dbHelper = new DBHelper(getContext());
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        //Создаем курсор
+        Cursor cursor = db.query("currency_name", null, null, null, null, null, null);
+
+        //Если БД не пустая
+        if (cursor.moveToFirst()) {
+            //Находим индексы колонок
+            int baseColId = cursor.getColumnIndex("currency_base");
+            int lastUsedColId = cursor.getColumnIndex("last_used");
+            int favoriteColId = cursor.getColumnIndex("favorite");
+
+            do {
+                //Создаем объект типа Currency и присваиваем ему значения из БД
+
+                //String name = cursor.getString(baseColId);
+
+                Currency currency = new Currency();
+                currency.setName(cursor.getString(baseColId));
+                currency.setLastUse(cursor.getInt(lastUsedColId));
+                int favorite = cursor.getInt(favoriteColId);
+
+                //В SQLite нет типа boolean, поэтому НЕ избранные валюты имеют в колонке favorite 0, а избранные 1
+                if (favorite == 0)
+                {
+                    currencies.add(currency);
+                    currency.setFavorite(false);
+                } else {
+                    currencies.add(0, currency);
+                    currency.setFavorite(true);
+                }
+
+                //Добавляем валюту в List с валютами
+
+            } while (cursor.moveToNext());
+        }
+
+
+        //Закрываем БД
+        db.close();
+
+        return currencies;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        Log.d(TAG, hashCode() + " onStartLoading");
+    }
+
+    @Override
+    protected void onStopLoading() {
+        super.onStopLoading();
+        Log.d(TAG, hashCode() + " onStopLoading");
+    }
+
+    @Override
+    protected void onAbandon() {
+        super.onAbandon();
+        Log.d(TAG, hashCode() + " onAbandon");
+    }
+
+    @Override
+    protected void onReset() {
+        super.onReset();
+        Log.d(TAG, hashCode() + " onReset");
+    }
+
+
+}
--- app/src/main/java/com/example/tfs_exchange/MainActivity.java
+++ app/src/main/java/com/example/tfs_exchange/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.tfs_exchange;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -12,7 +14,7 @@ import android.util.Log;
 
 import com.example.tfs_exchange.adapter.CurrencyRecyclerListAdapter;
 import com.example.tfs_exchange.comparators.FavoriteComparator;
-
+import android.content.Loader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -20,9 +22,13 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Currency>> {
+
+    public static final int LOADER_ID = 1;
 
     private final static String CURRENCY_TAG = "currency";
+
+    private final static String TAG = "MainActivity";
 
     private DBHelper dbHelper;
 
@@ -31,15 +37,23 @@ public class MainActivity extends AppCompatActivity {
 
     CurrencyRecyclerListAdapter adapter;
 
+    final List<Currency> currencies = new ArrayList<Currency>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        final List<Currency> currencies = new ArrayList<Currency>();
+        //final List<Currency> currencies = new ArrayList<Currency>();
 
-        populateCurrencies(currencies);
+        /** Загрузка валют из БД происходит асинхронно!**/
+        getLoaderManager().initLoader(LOADER_ID, null, this);
+        Loader<List<Currency>> loader;
+        loader = getLoaderManager().getLoader(LOADER_ID);
+        loader.forceLoad();
+
+        //populateCurrencies(currencies);
 
         //final RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
         adapter = new CurrencyRecyclerListAdapter(currencies, new CurrencyRecyclerListAdapter.OnItemClickListener() {
@@ -78,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setItemAnimator(itemAnimator);
     }
 
+    /**
     //Добавляем валюты в список из БД
     private void populateCurrencies(List<Currency> currencies){
 
@@ -123,6 +138,31 @@ public class MainActivity extends AppCompatActivity {
 
         //Закрываем БД
         db.close();
+    }
+     **/
+
+    @Override
+    public Loader<List<Currency>> onCreateLoader(int id, Bundle args) {
+        Loader<List<Currency>> loader = null;
+        if (id == LOADER_ID) {
+            loader = new AsyncDBLoader(this);
+            Log.d(TAG, "onCreateLoader: " + loader.hashCode());
+        }
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Currency>> loader, List<Currency> data) {
+        for (Currency currency : data) {
+            currencies.add(currency);
+        }
+
+        Log.d(TAG, "onLoadFinished: " + loader.hashCode());
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Currency>> loader) {
+        Log.d(TAG, "onLoaderReset for AsyncLoader " + loader.hashCode());
     }
 }
 
